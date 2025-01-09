f45d5f141983ddcbcdc0ca7774b8a9145fa77f76
Sneh Joshi <snehj814@gmail.com>
2025-01-09 14:13:04 +0530

    Update app_streamlit_new.py

--- app_streamlit_new.py
+++ app_streamlit_new.py
@@ -394,7 +394,7 @@ if uploaded_file:
                         [Only if figure exists - visualization analysis] Other wise, remove this section. Donot include this section if no visualization is present. 
 
                         ### Suggestions for Further Analysis
-                        [Bullet points of questions that can be asked on the data]
+                        [Bullet points of questions that can be asked on the data] - Make sure the suggestions should be related to the data given. Donot generate any random suggestions.
                         """
                         ).format(user_question=user_query,task=task,out_df=exec_locals["output_dict"],fig=str(graph_visual))
                         
