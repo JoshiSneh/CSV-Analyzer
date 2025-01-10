63744ed439d23b5e5142875c72076a363e13628d
Sneh Joshi <snehj814@gmail.com>
2025-01-10 19:53:22 +0530

    Update app_streamlit_new.py

--- app_streamlit_new.py
+++ app_streamlit_new.py
@@ -243,7 +243,6 @@ if uploaded_file:
                             - Use functions like pd.to_datetime() to convert columns when necessary.
                             - Add checks or use np.divide with where or np.errstate to handle division by zero safely.
                             - Use .str.strip() to remove leading and trailing spaces before comparisons or transformations.
-                            - When using lambda function make sure the code is correct and should not throw any errors.
                             - Avoid overly complex lambda functions; use named functions for clarity if the logic is complex.
                             - If for a operation a extraction of part is required from a string value then handle that carefully.
                             - For string extraction (e.g., using .str.extract()), ensure the regex pattern matches correctly and handles edge cases.
